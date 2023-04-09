651e27c1fa3a970f24fe318683c6a3add180921d
Frank Bell <frank@parity.io>
2023-04-09 12:59:41 +0100

    fix: improves relay chain selection, with expansion now only on arrow toggle

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,48 +21,70 @@ class HomePage extends StatelessWidget {
             child: Consumer<Chains>(
                 builder: (context, chains, child) => const ChainSyncStatus())),
         drawer: Drawer(
-            child: ListView(padding: EdgeInsets.zero, children: [
-          SizedBox(
-              height: MediaQuery.of(context).padding.top + kToolbarHeight + 8,
-              child: DrawerHeader(
-                decoration: const BoxDecoration(
-                  color: Colors.pink,
+            child: TextButtonTheme(
+                data: TextButtonThemeData(
+                  style: TextButton.styleFrom(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.zero,
+                      foregroundColor: Colors.grey.shade300),
                 ),
-                child: Text('Chains',
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: Colors.white, fontFamily: 'Syncopate-Bold')),
-              )),
-          // Add configured chains from provider
-          ...context.read<Chains>().chains.map((e) => ExpansionTile(
-                leading: e.logo,
-                title: Text(e.name,
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: Colors.black, fontFamily: 'Syncopate-Bold')),
-                initiallyExpanded: true,
-                children: e.parachains
-                    .map((e) => ListTile(
-                          leading: e.logo,
-                          title: Text(e.name,
+                child: ListView(padding: EdgeInsets.zero, children: [
+                  SizedBox(
+                      height: MediaQuery.of(context).padding.top +
+                          kToolbarHeight +
+                          8,
+                      child: DrawerHeader(
+                        decoration: const BoxDecoration(
+                          color: Colors.pink,
+                        ),
+                        child: Text('Chains',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .copyWith(
+                                    color: Colors.white,
+                                    fontFamily: 'Syncopate-Bold')),
+                      )),
+                  // Add configured chains from provider
+                  ...context.read<Chains>().chains.map((e) => ExpansionTile(
+                        title: TextButton.icon(
+                          icon: e.logo,
+                          label: Text(e.name,
                               style: Theme.of(context)
                                   .textTheme
                                   .titleMedium!
                                   .copyWith(
                                       color: Colors.black,
                                       fontFamily: 'Syncopate-Bold')),
-                          contentPadding: const EdgeInsets.only(left: 25),
-                          onTap: () async {
+                          onPressed: () async {
                             // Select the chain
                             await context.read<Chains>().select(e);
                             // Then close the drawer
                             Navigator.pop(context);
                           },
-                        ))
-                    .toList(),
-                onExpansionChanged: (bool expanded) async {
-                  // Select the chain
-                  await context.read<Chains>().select(e);
-                },
-              ))
-        ])));
+                        ),
+                        initiallyExpanded: true,
+                        children: e.parachains
+                            .map((e) => ListTile(
+                                  leading: e.logo,
+                                  title: Text(e.name,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium!
+                                          .copyWith(
+                                              color: Colors.black,
+                                              fontFamily: 'Syncopate-Bold')),
+                                  contentPadding:
+                                      const EdgeInsets.only(left: 25),
+                                  onTap: () async {
+                                    // Select the chain
+                                    await context.read<Chains>().select(e);
+                                    // Then close the drawer
+                                    Navigator.pop(context);
+                                  },
+                                ))
+                            .toList(),
+                      ))
+                ]))));
   }
 }
